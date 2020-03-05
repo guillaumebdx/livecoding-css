b7ecd788cd417e5ab0cdb22f26de9edb032f2d3c
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2020-03-05 16:32:37 +0100

    livecoding css done

---
+++ script.js
@@ -0,0 +1,6 @@
+let burger = document.getElementById('burger')
+let navbar = document.getElementById('navbar')
+
+burger.addEventListener('click', (event) => {
+    navbar.classList.toggle('open-burger');
+})
